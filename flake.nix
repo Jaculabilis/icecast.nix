268969d1d719a607981c9b6532aa4d7007e96b28
Tim Van Baak <tim.vanbaak@gmail.com>
2025-03-19 20:44:23 -0700

    Flake init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Custom icecast module";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-shell.url = "github:Mic92/nixos-shell";
+    nixos-shell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-shell,
+      ...
+    }:
+    let
+      inherit (nixpkgs.lib) nixosSystem;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
+}
